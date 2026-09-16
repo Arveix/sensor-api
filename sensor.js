f7fa6de4c699a0970f8340b84d88b018e2322f39
Arveix <arveix20@gmail.com>
2026-09-16 18:58:01 +0530

    Testing permissions for individual sensors

--- sensor.js
+++ sensor.js
@@ -1,9 +1,24 @@
-displayElem = document.getElementById('sensorData');
+// displayElem = document.getElementById('sensorData');
 
-try {
-    const sensor = new AbsoluteOrientationSensor({frequency: 60, referenceFrame: "device"});
-    displayElem.innerHTML = `Sensor data: ${JSON.stringify(sensor)}`;
-}
-catch(e) {
-    displayElem.innerHTML = e;
-}
+// try {
+//     const sensor = new AbsoluteOrientationSensor({frequency: 60, referenceFrame: "device"});
+//     displayElem.innerHTML = `Sensor data: ${JSON.stringify(sensor)}`;
+// }
+// catch(e) {
+//     displayElem.innerHTML = e;
+// }
+
+
+const sensor = new AbsoluteOrientationSensor();
+Promise.all([
+  navigator.permissions.query({ name: "accelerometer" }),
+  navigator.permissions.query({ name: "magnetometer" }),
+  navigator.permissions.query({ name: "gyroscope" }),
+]).then((results) => {
+  if (results.every((result) => result.state === "granted")) {
+    sensor.start();
+    // …
+  } else {
+    console.log("No permissions to use AbsoluteOrientationSensor.");
+  }
+});
